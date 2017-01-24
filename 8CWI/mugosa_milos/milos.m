5391bae05102a6411edadbeb12ce82de4a4b922a
MrRaisin <jraisin@christs.richmond.sch.uk>
2017-01-24 10:18:01 +0000

    Add files via upload

---
+++ 8CWI/mugosa_milos/milos.m
@@ -0,0 +1,25 @@
+<html>
+TOKYO!!!
+<head>
+			<title>Untitled</title>
+			<style>
+			
+</style>
+</head>
+
+<body>
+			<h1>Capital of Japan also Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. 
+			Tokyo is a major center for sports in Japan. Its professional sports teams compete in baseball, football (soccer), sumo and basketball. It hosted the 1964 Summer olympics.
+‎Overview · ‎Sports teams in Tokyo</h1>
+			<hr>
+			<p>	This is Tokyo !</p>
+				<img src=http://www.globetrotting.com.au/wp-content/uploads/2016/09/Tokyo_international-traveller-mag.jpg alt="">
+			<h2>london</h2>
+			<h3>London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. Across the Thames River, the London Eye observation wheel provides panoramic views of the South Bank cultural complex, and the entire city.</h3>
+			<img src=https://www.londonnightguide.com/wp-content/uploads/2014/12/London.jpg alt="">
+			<h4>Moscow</h4>
+			<h5>Moscow, on the Moskva River in western Russia, is the nation’s cosmopolitan capital. In its historic core is the Kremlin, a complex that’s home to the president and tsarist treasures in the Armoury. Outside its walls is Red Square, Russia's symbolic center. It's home to Lenin’s Mausoleum, the State Historical Museum's comprehensive collection and St. Basil’s Cathedral, known for its colorful, onion-shaped domes.</h5>
+			<img src= http://www.santour.ru/Moscow/images/moscow-7.jpg alt="">
+</body>
+
+</html>
